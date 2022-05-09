3dcffad454bc85a9ca29298a5e09f98e2ec97483
=Jean Moreira <jeanreal099@gmail.com>
2022-05-09 17:48:22 -0300

    add

--- src/main/java/br/com/unialfa/contasapagar/transaction/business/TransactionBusiness.java
+++ src/main/java/br/com/unialfa/contasapagar/transaction/business/TransactionBusiness.java
@@ -1,5 +1,32 @@
 package br.com.unialfa.contasapagar.transaction.business;
 
+import br.com.unialfa.contasapagar.transaction.domain.Transaction;
+import br.com.unialfa.contasapagar.transaction.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
 public class TransactionBusiness {
 
+    private final TransactionRepository transactionRepository;
+
+
+    public TransactionBusiness(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public void registerTransaction(Transaction transaction) {
+        Date date = new Date();
+        transaction.setCreated_at(date);
+        transactionRepository.save(transaction);
+    }
+
+//    private String getDateTime() {
+//        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+//        Date date = new Date();
+//        return dateFormat.format(date);
+//    }
 }
--- src/main/java/br/com/unialfa/contasapagar/transaction/service/TransactionController.java
+++ src/main/java/br/com/unialfa/contasapagar/transaction/service/TransactionController.java
@@ -1,4 +1,25 @@
 package br.com.unialfa.contasapagar.transaction.service;
 
+import br.com.unialfa.contasapagar.transaction.business.TransactionBusiness;
+import br.com.unialfa.contasapagar.transaction.domain.Transaction;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/transaction")
 public class TransactionController {
+
+    private final TransactionBusiness transactionBusiness;
+
+    public TransactionController(TransactionBusiness transactionBusiness) {
+        this.transactionBusiness = transactionBusiness;
+    }
+
+    @PostMapping(path = "/add")
+    public void registerTransaction(@RequestBody Transaction transaction) {
+        transactionBusiness.registerTransaction(transaction);
+    }
+
 }
